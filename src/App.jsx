748dcb86744e27b8d50c3cfeaf91346644dfcfef
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-06 13:52:28 +0000

    name

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const product ={
 }
   return (
    <>
-   
+   Farhan
    <div className="card" style={{width: "18rem"}}>
   <img className="card-img-top" src={product.image} />
   <div className="card-body">
