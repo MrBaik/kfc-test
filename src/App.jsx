ae4e9edf409eb7f5fd24c7e48ebff80237c155f6
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-06 13:59:24 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,65 @@
 
 //   let marks=[10,20,30,40,0,80,89,99];
 
-const product ={
+const product =[
+{
   
   id: 1,
+  title:"Hot Wings Bucket 1" ,
+  desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
+  price: 550,
+  image:"/kfc.png",
+  
+},
+{
+  
+  id: 2,
+  title:"Hot Wings Bucket 2" ,
+  desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
+  price: 550,
+  image:"/kfc.png",
+  
+},
+{
+  
+  id: 3,
+  title:"Hot Wings Bucket" ,
+  desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
+  price: 550,
+  image:"/kfc.png",
+  
+},
+{
+  
+  id: 4,
+  title:"Hot Wings Bucket" ,
+  desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
+  price: 550,
+  image:"/kfc.png",
+  
+},
+{
+  
+  id: 5,
+  title:"Hot Wings Bucket" ,
+  desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
+  price: 550,
+  image:"/kfc.png",
+  
+},
+{
+  
+  id: 6,
   title:"Hot Wings Bucket" ,
   desc:"KFc chicken worlds favorite get it now.KFc chicken worlds favorite get it now.",
   price: 550,
   image:"/kfc.png",
   
 }
+]
   return (
    <>
-   Farhan
+ 
    <div className="card" style={{width: "18rem"}}>
   <img className="card-img-top" src={product.image} />
   <div className="card-body">
@@ -24,7 +71,17 @@ const product ={
     <a href="#" className="btn btn-primary">Buy Now  </a>
   </div>
 </div>
+
+<div className="card" style={{width: "18rem"}}>
+  <img className="card-img-top" src={product.image} />
+  <div className="card-body">
+    <h5 className="card-title">{product.title}</h5>
+    <p className="card-text">{product.desc}</p>
+    <a href="#" className="btn btn-primary">Buy Now  </a>
+  </div>
+</div>
 </>
+
   )
 }
 
