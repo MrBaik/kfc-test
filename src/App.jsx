b1f2b6ba77f46c5a2fab8f98ebd840f0a1ce0607
Farhan Altaf <109102916+MrBaik@users.noreply.github.com>
2024-03-06 14:17:52 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -63,11 +63,10 @@ const product =[
   return (
    <>
 
-<div class="row">
-
-{products.map(products=>
+<div className="row">
+{products.map((products =>(
   
-  <div class="col-sm-6" key={product.id}>
+ <div class="col-sm-6" key={product.id}>
 
   <div className="card" style={{width: "18rem"}}>
  <img className="card-img-top" src={product.image} />
@@ -80,16 +79,50 @@ const product =[
 
 </div>
   
-  
-  
-  )}
+)  
+  ))}
 
 
   </div>
 </>
 
-  )
+  );
 }
 
 
 export default App;
+
+
+
+
+
+// // array of objects
+// function App() {
+//   return (
+//     <div className="container my-5">
+//     <div className="row row-cols-1 row-cols-md-4 g-4">
+//       {products.map((product) => (
+//         <div className="col" key={product.id}>
+//           <div className="card">
+//             <img src={product.image} className="card-img-top" alt="..." />
+//             <div className="card-body">
+//               <h5 className="card-title">
+//                 {product.title}{" "}
+//                 <span className="badge text-bg-primary">
+//                   Rs. {product.price}
+//                 </span>
+//               </h5>
+//               <p className="card-text">{product.desc}</p>
+//               <a href="#" className="btn btn-primary">
+//                 Add to Cart
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//     </div>
+//   );
+// }
+
+// export default App;
